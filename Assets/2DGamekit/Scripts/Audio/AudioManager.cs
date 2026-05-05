027d7c2d10801e379047ffc480aa394a7663ecfc
OscDaTal <oscar@tallert.se>
2026-05-05 14:59:32 +0200

    Ändrat grjer

--- Assets/2DGamekit/Scripts/Audio/AudioManager.cs
+++ Assets/2DGamekit/Scripts/Audio/AudioManager.cs
@@ -21,6 +21,7 @@ public class AudioManager : MonoBehaviour
         public StudioEventEmitter musicBoss;
         public StudioEventEmitter musicPause;
         public StudioEventEmitter snapShotPause;
+        public StudioEventEmitter ambiance;
     }
     public Emitters eventEmitters;
 
@@ -31,6 +32,8 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private EventReference playerJump;
     [SerializeField] private EventReference playerLand;
     [SerializeField] private EventReference playerAttackMelee;
+    [SerializeField] private EventReference playerAttackRanged;
+
     
     [SerializeField] private EventReference playerHurt;
     EventInstance playerFootstepInstance;
@@ -224,6 +227,16 @@ public class AudioManager : MonoBehaviour
             return;
         }
         RuntimeManager.PlayOneShot(playerAttackMelee, transform.position);
+    }
+    
+    public void PlayRanged()
+    {
+        if (playerAttackRanged.IsNull)
+        {
+            Debug.LogWarning("Fmod event not found: playerAttackRanged");
+            return;
+        }
+        RuntimeManager.PlayOneShot(playerAttackRanged, transform.position);
     }
 
 	
